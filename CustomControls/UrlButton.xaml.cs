6b9f7701c3ff32615bdbb763743d8b52c8530fd1
Kopchigashev-Ilya <ilya.kopchigashev@gmail.com>
2023-03-11 00:35:30 +0300

    Add home page.

---
+++ CustomControls/UrlButton.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnailPass_Desktop.CustomControls
+{
+    public partial class UrlButton : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(UrlButton), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Path), typeof(UrlButton), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty UrlProperty =
+            DependencyProperty.Register("Url", typeof(string), typeof(UrlButton), new PropertyMetadata(string.Empty));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Path Icon
+        {
+            get { return (Path)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        public UrlButton()
+        {
+            InitializeComponent();
+        }
+
+        private void Link_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Url,
+                UseShellExecute = true
+            });
+        }
+    }
+}
